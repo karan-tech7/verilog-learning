0eab9d245d9f5f5bdbdbf3069c63f5f803a9b325
Karan M <karanxtech7@gmail.com>
2026-08-25 11:01:59 +0530

    Create xor_gate_tb.v

---
+++ logic-gates/xor_gate_tb.v
@@ -0,0 +1,33 @@
+module xor_gate_tb;
+
+reg a,b;
+
+wire y;
+
+xor_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+ initial begin
+
+ a=0; b=0; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=0; b=1; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=1; b=0; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=1; b=1; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ end
+
+ endmodule
