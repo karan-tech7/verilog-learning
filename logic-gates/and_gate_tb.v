cf31cdcacfffd971b4be9fae4d056e916e3c4473
Karan M <karanxtech7@gmail.com>
2026-08-25 10:50:53 +0530

    Create and_gate_tb.v

---
+++ logic-gates/and_gate_tb.v
@@ -0,0 +1,30 @@
+module and_gate_tb;
+
+reg a, b;
+wire y;
+
+and_gate uut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $display("a b y");
+
+    a=0; b=0; #10;
+    $display("%b %b %b", a, b, y);
+
+    a=0; b=1; #10;
+    $display("%b %b %b", a, b, y);
+
+    a=1; b=0; #10;
+    $display("%b %b %b", a, b, y);
+
+    a=1; b=1; #10;
+    $display("%b %b %b", a, b, y);
+
+    $finish;
+end
+
+endmodule
