11765f34eb0f3363a4a67c325ed497dab746485d
Karan M <karanxtech7@gmail.com>
2026-08-25 11:00:06 +0530

    Create nor_gate_tb.v

---
+++ logic-gates/nor_gate_tb.v
@@ -0,0 +1,33 @@
+module nor_gate_tb;
+
+reg a,b;
+
+wire y;
+
+nor_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+ initial begin
+
+ a=0; b=0; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=0; b=1; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=1; b=0; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ a=1; b=1; #10;
+
+ $display("%b %b %b",a,b,y);
+
+ end
+
+ endmodule
