093e7c114c05744bf873bfdd4cad1fe3cb26260d
Karan M <karanxtech7@gmail.com>
2026-08-25 10:57:42 +0530

    Create nand_gate_tb.v

---
+++ logic-gates/nand_gate_tb.v
@@ -0,0 +1,31 @@
+module nand_gate_tb;
+
+reg a,b;
+wire y;
+
+nand_gate uut(
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    a=0; b=0; #10;
+    
+    $display("%b %b %b",a , b , y);
+    
+    a=0; b=1; #10;
+
+    $display("%b %b %b", a, b, y);
+
+    a=1; b=0; #10;
+
+    $display("%b %b %b", a,b,y);
+
+    a=1; b=1; #10;
+
+    $display("%b %b %b",a,b,y);
+
+end
+
+endmodule
