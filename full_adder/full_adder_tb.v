42aeced4c91bf07232afe27ba96c8abdf89645be
Karan M <karanxtech7@gmail.com>
2026-09-04 08:42:17 +0530

    Create full_adder_tb.v

---
+++ full_adder/full_adder_tb.v
@@ -0,0 +1,28 @@
+module full_adder_tb;
+reg a;
+reg b;
+reg cin;
+wire sum;
+wire carry;
+full_adder uut(
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .sum(sum),
+    .cout(cout)
+);
+initial begin
+$monitor("a=%b b=%b cin=%b | sum=%b cout =%b",a,b,cin,sum,cout);
+$dumpfile("full_adder.vcd");
+$dumpvars(0,full_adder_tb);
+a=0; b=0; cin=0;#10;
+a=0; b=0; cin=1;#10;
+a=1; b=1; cin=0;#10;
+a=1; b=0; cin=0;#10;
+a=1; b=0; cin=1;#10;
+a=1; b=1; cin=0;#10;
+a=1; b=1; cin=1;#10;
+
+$finish;
+end
+endmodule
